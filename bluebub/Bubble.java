c2b94e3bb26d8a58f94c9b80cd87b292405fd639
Luca Mayer <mayer@zeppel.eu>
2019-05-18 12:47:44 +0200

    Added quadratics
    Added quadratics
    Added manual size for bubble
    Added manual quadratic size

--- bluebub/Bubble.java
+++ bluebub/Bubble.java
@@ -9,291 +9,350 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
 /**
- * 
+ *
  * @author zeppel
  * @web https://zeppel.eu
  * @github github.com/zeppelsoftware
- * 
+ *
  * @version 1
- * 
+ *
  *         Library for creating nice chatbubbles in JavaFX Copyright zeppel 2019
- * 
+ *
  *         Requirements: JavaFX
  *
  */
 public class Bubble extends Group {
 
-	private int p = 14;
-	private int s = 2; 
-	private int pm = 10; 
-	private int sm = 2;
-	private Font textFont = Font.font("Arial", 18);
-	private Paint textColor = Color.WHITE; 
-	private Font metaFont = Font.font("Arial", 12);
-	private Paint metaColor = Color.LIGHTGRAY; 
+    private int p = 14;
+    private int s = 2;
+    private int pm = 10;
+    private int sm = 2;
+    private Font textFont = Font.font("Arial", 18);
+    private Paint textColor = Color.WHITE;
+    private Font metaFont = Font.font("Arial", 12);
+    private Paint metaColor = Color.LIGHTGRAY;
 
-	private Paint bubbleColor = Color.rgb(0, 126, 229); 
-	
-	private int edgeRadius = 30;
+    private Rectangle r;
 
-	/**
-	 * create a chat bubble
-	 * 
-	 * @param text
-	 */
-	public Bubble(String text) {
-		super();
-		init(0, 0, text, "");
-	}
+    private Paint bubbleColor = Color.rgb(0, 126, 229);
 
-	/**
-	 * create a chat bubble
-	 * 
-	 * @param text
-	 * @param meta
-	 */
-	public Bubble(String text, String meta) {
-		super();
-		init(0, 0, text, meta);
-	}
+    private int edgeRadius = 30;
 
-	/**
-	 * create a chat bubble
-	 * 
-	 * @param x
-	 * @param y
-	 * @param text
-	 * @param meta
-	 */
-	public Bubble(int x, int y, String text, String meta) {
-		super();
-		init(x, y, text, meta);
-	}
+    /**
+     * create a chat bubble
+     *
+     * @param text
+     */
+    public Bubble(String text) {
+        super();
+        init(0, 0, text, "");
+    }
 
-	private void init(int x, int y, String text, String meta) {
-		// temp for text
-		Text temp = new Text(text);
-		temp.setFont(textFont);
-		int textW = (int) temp.getLayoutBounds().getWidth();
-		int textH = (int) temp.getLayoutBounds().getHeight();
-		int w = textW + p * 2 + s * 2;
-		int h = textH + p * 2;
-		temp = null;
+    /**
+     * create a quadratic chat bubble
+     *
+     * @param text
+     */
+    public Bubble(String text, String meta, boolean quadratic) {
+        super();
+        if(quadratic == true) {
+            init(0, 0, text, meta);
+            r.setHeight(r.getWidth());
+        }
+        else
+            init(0, 0, text, meta);
+    }
 
-		// tmp for meta
-		Text tmp = new Text(meta);
-		tmp.setFont(metaFont);
-		int metaW = (int) tmp.getLayoutBounds().getWidth();
-		int metaH = (int) tmp.getLayoutBounds().getHeight();
-		h += metaH;
-		temp = null;
+    /**
+     * create a quadratic chat bubble
+     * 
+     * @param text
+     */
+    public Bubble(String text, boolean quadratic) {
+        super();
+        if(quadratic == true) {
+            init(0, 0, text, "");
+            r.setHeight(r.getWidth());
+        }
+        else
+            init(0, 0, text, "");
+    }
 
-		// label text
-		Label l = new Label(text);
-		l.setFont(textFont);
-		l.setTextFill(textColor);
-		l.setTranslateX(x + p + s);
-		l.setTranslateY(y + p);
+    /**
+     * create a chat bubble
+     *
+     * @param text
+     * @param meta
+     */
+    public Bubble(String text, String meta) {
+        super();
+        init(0, 0, text, meta);
+    }
 
-		// label meta
-		Label m = new Label(meta);
-		m.setFont(metaFont);
-		m.setTextFill(metaColor);
-		m.setTranslateX(x + (w - (metaW + pm + sm)));
-		m.setTranslateY(y + textH + pm * 2);
+    /**
+     * create a chat bubble
+     *
+     * @param x
+     * @param y
+     * @param text
+     * @param meta
+     */
+    public Bubble(int x, int y, String text, String meta) {
+        super();
+        init(x, y, text, meta);
+    }
 
-		// bubble
-		Rectangle r = new Rectangle();
-		r.setTranslateX(x);
-		r.setTranslateY(y);
+    private void init(int x, int y, String text, String meta) {
+        // temp for text
+        Text temp = new Text(text);
+        temp.setFont(textFont);
+        int textW = (int) temp.getLayoutBounds().getWidth();
+        int textH = (int) temp.getLayoutBounds().getHeight();
+        int w = textW + p * 2 + s * 2;
+        int h = textH + p * 2;
+        temp = null;
 
-		r.setWidth(w);
-		r.setHeight(h);
+        // tmp for meta
+        Text tmp = new Text(meta);
+        tmp.setFont(metaFont);
+        int metaW = (int) tmp.getLayoutBounds().getWidth();
+        int metaH = (int) tmp.getLayoutBounds().getHeight();
+        h += metaH;
+        temp = null;
 
-		r.setArcHeight(this.edgeRadius);
-		r.setArcWidth(this.edgeRadius);
+        // label text
+        Label l = new Label(text);
+        l.setFont(textFont);
+        l.setTextFill(textColor);
+        l.setTranslateX(x + p + s);
+        l.setTranslateY(y + p);
 
-		r.setFill(bubbleColor);
+        // label meta
+        Label m = new Label(meta);
+        m.setFont(metaFont);
+        m.setTextFill(metaColor);
+        m.setTranslateX(x + (w - (metaW + pm + sm)));
+        m.setTranslateY(y + textH + pm * 2);
 
-		getChildren().addAll(r, l, m);
-	}
-	/**
-	* set the radius of the bubble edges
-	* @param radius
-	*/
-	public void setEdgeRadius(int radius) {
-		this.edgeRadius = radius;
-	}
-	
-	/**
-	* get the radius of the bubble edges
-	*
-	* @return
-	*/
-	public int getEdgeRadius() {
-		return this.edgeRadius;	
-	}
+        // bubble
+        r = new Rectangle();
+        r.setTranslateX(x);
+        r.setTranslateY(y);
 
-	/**
-	 * get the padding of the text
-	 * 
-	 * @return
-	 */
-	public int getTextPadding() {
-		return p;
-	}
+        r.setWidth(w);
+        r.setHeight(h);
 
-	/**
-	 * set the padding of the text
-	 * 
-	 * @param textPadding
-	 */
-	public void setTextPadding(int textPadding) {
-		this.p = textPadding;
-	}
+        r.setArcHeight(this.edgeRadius);
+        r.setArcWidth(this.edgeRadius);
 
-	/**
-	 * get the sidespace of the text
-	 * 
-	 * @return
-	 */
-	public int getTextSidespace() {
-		return s;
-	}
+        r.setFill(bubbleColor);
 
-	/**
-	 * set the sidespace of the text
-	 * 
-	 * @param textSidespace
-	 */
-	public void setTextSidespace(int textSidespace) {
-		this.s = textSidespace;
-	}
+        getChildren().addAll(r, l, m);
+    }
 
-	/**
-	 * get the padding of the meta text
-	 * 
-	 * @return
-	 */
-	public int getMetaPadding() {
-		return pm;
-	}
 
-	/**
-	 * set the padding of the meta text
-	 * 
-	 * @param metaPadding
-	 */
-	public void setMetaPadding(int metaPadding) {
-		this.pm = metaPadding;
-	}
+    /**
+     * set bubble width
+     * @param width
+     */
+    public void setWidth(double width) {
+        r.setWidth(width);
+    }
 
-	/**
-	 * get the sidespace of the meta text
-	 * 
-	 * @return
-	 */
-	public int getMetaSidespace() {
-		return sm;
-	}
+    /**
+     * set bubble height
+     * @param height
+     */
+    public void setHeight(double height) {
+        r.setWidth(height);
+    }
 
-	/**
-	 * set the sidespace of the meta text
-	 * 
-	 * @param metaSidespace
-	 */
-	public void setMetaSidespace(int metaSidespace) {
-		this.sm = metaSidespace;
-	}
+    /**
+     * Set bubble quadratic size
+     * @param size
+     */
+    public void setQuadraticSize(double size) {
+        r.setWidth(size);
+        r.setHeight(size);
+    }
 
-	/**
-	 * get the font of the text
-	 * 
-	 * @return
-	 */
-	public Font getTextFont() {
-		return textFont;
-	}
+    /**
+     * set the radius of the bubble edges
+     * @param radius
+     */
+    public void setEdgeRadius(int radius) {
+        this.edgeRadius = radius;
+    }
 
-	/**
-	 * set the font of the text
-	 * 
-	 * @param textFont
-	 */
-	public void setTextFont(Font textFont) {
-		this.textFont = textFont;
-	}
+    /**
+     * get the radius of the bubble edges
+     *
+     * @return
+     */
+    public int getEdgeRadius() {
+        return this.edgeRadius;
+    }
 
-	/**
-	 * get the color of the text
-	 * 
-	 * @return
-	 */
-	public Paint getTextColor() {
-		return textColor;
-	}
+    /**
+     * get the padding of the text
+     *
+     * @return
+     */
+    public int getTextPadding() {
+        return p;
+    }
 
-	/**
-	 * set the color of the text
-	 * 
-	 * @param textColor
-	 */
-	public void setTextColor(Paint textColor) {
-		this.textColor = textColor;
-	}
+    /**
+     * set the padding of the text
+     *
+     * @param textPadding
+     */
+    public void setTextPadding(int textPadding) {
+        this.p = textPadding;
+    }
 
-	/**
-	 * get the font of the meta text
-	 * 
-	 * @return
-	 */
-	public Font getMetaFont() {
-		return metaFont;
-	}
+    /**
+     * get the sidespace of the text
+     *
+     * @return
+     */
+    public int getTextSidespace() {
+        return s;
+    }
 
-	/**
-	 * set the font of the meta text
-	 * 
-	 * @param metaFont
-	 */
-	public void setMetaFont(Font metaFont) {
-		this.metaFont = metaFont;
-	}
+    /**
+     * set the sidespace of the text
+     *
+     * @param textSidespace
+     */
+    public void setTextSidespace(int textSidespace) {
+        this.s = textSidespace;
+    }
 
-	/**
-	 * get the color of the meta text
-	 * 
-	 * @return
-	 */
-	public Paint getMetaColor() {
-		return metaColor;
-	}
+    /**
+     * get the padding of the meta text
+     *
+     * @return
+     */
+    public int getMetaPadding() {
+        return pm;
+    }
 
-	/**
-	 * set the color of the meta text
-	 * 
-	 * @param metaColor
-	 */
-	public void setMetaColor(Paint metaColor) {
-		this.metaColor = metaColor;
-	}
+    /**
+     * set the padding of the meta text
+     *
+     * @param metaPadding
+     */
+    public void setMetaPadding(int metaPadding) {
+        this.pm = metaPadding;
+    }
 
-	/**
-	 * get the color of the bubble
-	 * 
-	 * @return
-	 */
-	public Paint getBubbleColor() {
-		return bubbleColor;
-	}
+    /**
+     * get the sidespace of the meta text
+     *
+     * @return
+     */
+    public int getMetaSidespace() {
+        return sm;
+    }
 
-	/**
-	 * Set the color of the bubble
-	 * 
-	 * @param bubbleColor
-	 */
-	public void setBubbleColor(Paint bubbleColor) {
-		this.bubbleColor = bubbleColor;
-	}
+    /**
+     * set the sidespace of the meta text
+     *
+     * @param metaSidespace
+     */
+    public void setMetaSidespace(int metaSidespace) {
+        this.sm = metaSidespace;
+    }
+
+    /**
+     * get the font of the text
+     *
+     * @return
+     */
+    public Font getTextFont() {
+        return textFont;
+    }
+
+    /**
+     * set the font of the text
+     *
+     * @param textFont
+     */
+    public void setTextFont(Font textFont) {
+        this.textFont = textFont;
+    }
+
+    /**
+     * get the color of the text
+     *
+     * @return
+     */
+    public Paint getTextColor() {
+        return textColor;
+    }
+
+    /**
+     * set the color of the text
+     *
+     * @param textColor
+     */
+    public void setTextColor(Paint textColor) {
+        this.textColor = textColor;
+    }
+
+    /**
+     * get the font of the meta text
+     *
+     * @return
+     */
+    public Font getMetaFont() {
+        return metaFont;
+    }
+
+    /**
+     * set the font of the meta text
+     *
+     * @param metaFont
+     */
+    public void setMetaFont(Font metaFont) {
+        this.metaFont = metaFont;
+    }
+
+    /**
+     * get the color of the meta text
+     *
+     * @return
+     */
+    public Paint getMetaColor() {
+        return metaColor;
+    }
+
+    /**
+     * set the color of the meta text
+     *
+     * @param metaColor
+     */
+    public void setMetaColor(Paint metaColor) {
+        this.metaColor = metaColor;
+    }
+
+    /**
+     * get the color of the bubble
+     *
+     * @return
+     */
+    public Paint getBubbleColor() {
+        return bubbleColor;
+    }
+
+    /**
+     * Set the color of the bubble
+     *
+     * @param bubbleColor
+     */
+    public void setBubbleColor(Paint bubbleColor) {
+        this.bubbleColor = bubbleColor;
+    }
 
 }
